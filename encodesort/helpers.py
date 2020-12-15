81041c1ada78627fe950a95cce01592df7c93fda
sz <sz@galacticicecube.com>
2020-12-15 03:23:15 -0600

    example sort encode+decode for python 3.6+ dict

---
+++ encodesort/helpers.py
@@ -0,0 +1,14 @@
+
+from .encodesort import encode, decode
+
+
+def dict_encode(d, bits):
+    d = {k: d[k] for k in sorted(d)}
+
+    indices = list(sorted(d))
+    indices = encode(indices, bits)
+    return {k: d[k] for k in indices}
+
+
+def dict_decode(d):
+    return decode(list(d.keys()))
